1fa87f4ba99934eb8ca43af91ba28b9068422bb1
Albarell <Albarell>
2019-12-13 23:57:38 +0100

    animation audio

---
+++ Animation/Cinematique/bras.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bras : MonoBehaviour
+{
+    public GameObject Arms;
+	public float VitesseRotation,VitesseDeplacement;
+	
+    void Update()
+    {
+        gameObject.transform.eulerAngles = new Vector3(Mathf.LerpAngle(gameObject.transform.eulerAngles.x,Arms.transform.eulerAngles.x,Time.deltaTime * VitesseRotation),Mathf.LerpAngle(gameObject.transform.eulerAngles.y,Arms.transform.eulerAngles.y,Time.deltaTime * VitesseRotation),Mathf.LerpAngle(gameObject.transform.eulerAngles.z,Arms.transform.eulerAngles.z,Time.deltaTime * VitesseRotation));
+		gameObject.transform.position = new Vector3(Mathf.Lerp(gameObject.transform.position.x,Arms.transform.position.x,Time.deltaTime * VitesseDeplacement),Mathf.Lerp(gameObject.transform.position.y,Arms.transform.position.y,Time.deltaTime * VitesseDeplacement),Mathf.Lerp(gameObject.transform.position.z,Arms.transform.position.z,Time.deltaTime * VitesseDeplacement));
+    }
+}
---
+++ Animation/Cinematique/marche.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class marche : MonoBehaviour
+{
+    public bool Marche;
+	public float vitesse,positionY,coeffHeadUp;
+	public float varLerp;
+	private float vvLerp;
+	public AudioSource m_AudioSource;
+	public AudioClip[] bruitDePas;
+	private Vector3 posObject;
+	private bool lerpb;
+	
+	void Start()
+	{
+		positionY = gameObject.transform.position.y;
+		//StartCoroutine(pas());
+	}
+
+    void LateUpdate()
+    {
+        if(Marche)
+		{
+			Vector3 posObject = gameObject.transform.position;
+			varLerp = Mathf.SmoothStep(Mathf.PingPong(Time.time * vitesse, coeffHeadUp),varLerp,Time.deltaTime * vitesse);
+			if(varLerp > vvLerp)
+			{
+				lerpb = true;
+			}
+			else
+			{
+				if(lerpb)
+				{
+					int rn = Random.Range(0,bruitDePas.Length);
+					m_AudioSource.clip = bruitDePas[rn];
+					m_AudioSource.PlayOneShot(m_AudioSource.clip);
+					lerpb = false;
+				}
+			}
+			vvLerp = varLerp;
+			posObject = new Vector3(posObject.x,varLerp + positionY,posObject.z);
+			gameObject.transform.position = posObject;
+		}
+    }
+
+	IEnumerator pas()
+	{
+		while(true)
+		{
+			if(Marche)
+			{
+				int rn = Random.Range(0,bruitDePas.Length);
+				m_AudioSource.clip = bruitDePas[rn];
+				m_AudioSource.PlayOneShot(m_AudioSource.clip);
+			}
+			yield return new WaitForSeconds(vitesse);
+		}
+	}
+}
